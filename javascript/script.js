5826017efa9c1b75456ad76561f38b2faf89a161
Emily Kelton <emilyk000@gmail.com>
2014-07-01 23:28:04 -0700

    jquery to get input value working. Jquery to add li + textbox working.

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,29 @@
 $(document).ready(function(){
-		alert("hi");
-	// $('input').keydown(function(e) {
- //  if (e.keyCode == 13) {
- //  $(this).closest('form').submit(function(){
- //  	alert("hi");
- //  });
- //  }
-// });
 
-	// $("#item").submit(function() {
-	// 	alert("hello world");
-	// });
-$("#item").keypress (function(e) {
+	$("#item").keypress (function(e) {
         if (e.keyCode == 13) {
-            alert("Enter pressed");
-        };
+            // alert("Enter pressed");
+
+						if($("#item").val()=='') {
+       					alert("please enter some shit to get done");
+       				}
+       			else {
+
+       				var new_task = $('#item').val();
+       				alert(new_task);
+       				$("#list ul").append('<li><input type="text"></li>').val(new_task)
+       				// $("this").addClass(".notdone")
+       			};
+          // $("#change").addClass("button-disabled").removeClass("button");
+          // $("#change").attr("disabled","disabled");
+          //  }
+          //  else
+          //   {
+          //  $("#change").removeClass("button-disabled").addClass("button");
+          //  $("#change").removeAttr("disabled");
+          //   }
+          //   }
+    		 };
+ 
 	});
 });
