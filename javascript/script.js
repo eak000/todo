c6de61d880aaf4b7ebc9caff66d602060daf729b
Emily Kelton <emilyk000@gmail.com>
2014-07-02 21:07:45 -0700

    got input box clearning after enter

--- javascript/script.js
+++ javascript/script.js
@@ -2,21 +2,19 @@ $(document).ready(function(){
 
 	$("#item").keypress (function(e) {
         if (e.keyCode == 13) {
-            // alert("Enter pressed");
+
             event.preventDefault();
 
+            //make sure the input isn't blank
 						if($("#item").val()=='') {
        					alert("please enter some sh!t to get done");
        				}
+
        			else {
-
-       				// var new_task = $('#item').val();
-       				// var new_item = $("#list ul").append('<li class="notdone"><span class="list_item"></span></li>');
-           //    var new_content = $(".list_item").text(new_task);
-
+              //add list item and give it notdone style
        				$("#list ul").append('<li class="notdone"><span class="list_item">'+ $('#item').val() + '</span></li>');              
-       				// $("new_item").text(new_task);
-              
+       				// clear input box
+              $("#item").val("");
        		     };
         
     		  };
