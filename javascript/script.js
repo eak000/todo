5204614dbde42d2da56f128510f89a3f21acd8fc
Emily Kelton <emilyk000@gmail.com>
2014-07-02 20:17:34 -0700

    list items no longer overwriting

--- javascript/script.js
+++ javascript/script.js
@@ -3,21 +3,23 @@ $(document).ready(function(){
 	$("#item").keypress (function(e) {
         if (e.keyCode == 13) {
             // alert("Enter pressed");
+            event.preventDefault();
 
 						if($("#item").val()=='') {
        					alert("please enter some sh!t to get done");
        				}
        			else {
 
-       				var new_task = $('#item').val();
-       				alert(new_task);
-       				$("#list ul").append('<li class="notdone"><span class="list_item"></span></li>');              // $(".list_item").
-       				$(".list_item").text(new_task);
-              // $(".list_item").addClass("notdone");
+       				// var new_task = $('#item').val();
+       				// var new_item = $("#list ul").append('<li class="notdone"><span class="list_item"></span></li>');
+           //    var new_content = $(".list_item").text(new_task);
+
+       				$("#list ul").append('<li class="notdone"><span class="list_item">'+ $('#item').val() + '</span></li>');              
+       				// $("new_item").text(new_task);
               
-       			};
+       		     };
         
-    		 };
+    		  };
  
-	});
+	 });
 });
