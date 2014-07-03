9abe0889ceea0c9eb42b0a2b4134011e704487f7
Emily Kelton <emilyk000@gmail.com>
2014-07-02 17:52:09 -0700

    got before and after list item styles working

--- javascript/script.js
+++ javascript/script.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
             // alert("Enter pressed");
 
 						if($("#item").val()=='') {
-       					alert("please enter some shit to get done");
+       					alert("please enter some sh!t to get done");
        				}
        			else {
 
        				var new_task = $('#item').val();
        				alert(new_task);
-       				$("#list ul").append('<li><span class="list_item"></span></li>');              // $(".list_item").
+       				$("#list ul").append('<li class="notdone"><span class="list_item"></span></li>');              // $(".list_item").
        				$(".list_item").text(new_task);
-              $(".list_item").addClass("notdone");
+              // $(".list_item").addClass("notdone");
               
        			};
         
