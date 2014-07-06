91396af16e67cf523c98c089c1bb2847166b7139
Emily Kelton <emilyk000@gmail.com>
2014-07-06 15:54:59 -0700

    got plus sign adding lines

--- javascript/script.js
+++ javascript/script.js
@@ -1,27 +1,41 @@
 $(document).ready(function(){
 
+  $("#addToList").click(function(){
+      addItems();
+})
+
+      var uncheck= "&#xe603;"
+      var check= "&#xe600;"
+
 	$("#item").keypress (function(e) {
+
+
         if (e.keyCode == 13) {
+          addItems();
 
-            event.preventDefault();
-
-            //make sure the input isn't blank
-						if($("#item").val()=='') {
-       					alert("please enter some sh!t to get done");
-       				}
-
-       			else {
-              //add list item and give it notdone style
-       				$("#list ul").append('<li class="notdone"><span aria-hidden="true" data-icon="&#xe603;" class="unchecked"></span><span class="list_item">'+ $('#item').val() + '</span><span aria-hidden="true" data-icon="&#xe601;" class="delete"></span></li>');              
-       				// clear input box
-              $("#item").val("");
-       		     };
     		  };
        
        $(".unchecked").click(function() {
-          alert("you clicked the check!");
+          // alert($(this));
+
+          $(this).attr('data-icon', check).addClass('ui-icon-check');
+
         });
 	 });
+        var addItems =function() {
+              event.preventDefault();
 
+            //make sure the input isn't blank
+            if($("#item").val()=='') {
+                alert("please enter some sh!t to get done");
+              }
+
+            else {
+              //add list item and give it notdone style
+              $("#list ul").append('<li class="notdone"><span aria-hidden="true" data-icon= '+ uncheck +' class="unchecked"></span><span class="list_item">'+ $('#item').val() + '</span><span aria-hidden="true" data-icon="&#xe601;" class="delete"></span></li>');              
+              // clear input box
+              $("#item").val("");
+               };
+        };
 
 });
